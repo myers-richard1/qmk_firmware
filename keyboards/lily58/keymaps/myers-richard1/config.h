5cbfc535e92f182cea20e1d96fb7567ff2bca6de
Richard Myers <myers_richard1@columbusstate.edu>
2021-05-06 17:10:57 -0400

    Set up numpad to be tap-toggle/hold

--- keyboards/lily58/keymaps/myers-richard1/config.h
+++ keyboards/lily58/keymaps/myers-richard1/config.h
@@ -26,8 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
+#define TAPPING_TOGGLE 2
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
--- keyboards/lily58/keymaps/myers-richard1/keymap.c
+++ keyboards/lily58/keymaps/myers-richard1/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_EQL, \
   KC_LCTRL, KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                     KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                        KC_LWIN, MO(_LOWER), KC_BSPC, KC_DEL, KC_ENT, KC_SPC, KC_LALT, MO(_RAISE) \
+                        KC_LWIN, TT(_LOWER), KC_BSPC, KC_DEL, KC_ENT, KC_SPC, KC_LALT, MO(_RAISE) \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.

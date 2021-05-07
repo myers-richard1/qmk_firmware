d2e54835a3cccd0d51fcbc1d69bc1e370055de59
Richard Myers <myers_richard1@columbusstate.edu>
2021-05-07 18:22:02 -0400

    Clean up formatting on lower layer

--- keyboards/lily58/keymaps/myers-richard1/keymap.c
+++ keyboards/lily58/keymaps/myers-richard1/keymap.c
@@ -51,11 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //this is the layout used when the left modifier is down
 [_LOWER] = LAYOUT( \
-  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                   KC_F7, KC_F8, KC_F9,KC_F10, KC_F11, KC_F12,\
-  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                     XXXXXXX,   KC_7,   KC_8,   KC_9,  XXXXXXX,  XXXXXXX, \
-  KC_CLCK, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,                   XXXXXXX, KC_4, KC_5, KC_6, XXXXXXX, XXXXXXX, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_1, KC_2, KC_3, XXXXXXX, KC_RSFT, \
-                             KC_LWIN, _______, XXXXXXX, XXXXXXX, XXXXXXX,  KC_0, KC_LALT, XXXXXXX\
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8, KC_F9, KC_F10, KC_F11,  KC_F12,\
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_7,  KC_8,  KC_9,   XXXXXXX, XXXXXXX, \
+  KC_CLCK, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_4,  KC_5,  KC_6,   XXXXXXX, XXXXXXX, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_1,  KC_2,  KC_3,   XXXXXXX, KC_RSFT, \
+                        KC_LWIN, _______, XXXXXXX, XXXXXXX,         XXXXXXX,  KC_0, KC_LALT, XXXXXXX\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.

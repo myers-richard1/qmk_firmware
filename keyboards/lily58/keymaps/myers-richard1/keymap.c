4e034c0909457cba52453777423300df7139afee
Richard Myers <myers_richard1@columbusstate.edu>
2021-07-23 21:12:00 -0400

    Remap win in ffxiv layer to something mappable (J)

--- keyboards/lily58/keymaps/myers-richard1/keymap.c
+++ keyboards/lily58/keymaps/myers-richard1/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  KC_F6,   KC_F2,   KC_W,    KC_F,    KC_P,    KC_G,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_BSLASH, \
  KC_F7,   KC_F3,   KC_R,    KC_S,    KC_T,    KC_D,                     KC_H,    KC_N,    KC_E,    KC_I,    KC_O,     KC_QUOT, \
  KC_F8,   KC_F4,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  TG(_FFXIV),  KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                       KC_LWIN,KC_LCTRL, KC_LSFT, KC_LALT, KC_ENT, KC_SPC, KC_LALT, XXXXXXX \
+                       KC_J,KC_LCTRL, KC_LSFT, KC_LALT, KC_ENT, KC_SPC, KC_LALT, XXXXXXX \
 )
 };
 
